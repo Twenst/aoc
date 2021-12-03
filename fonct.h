2e3713c66a586c50e8f81fe15a80ec0b891dbf44
Twens <56125618+Twenst@users.noreply.github.com>
2021-12-03 23:01:14 +0100

    Update fonct.h

--- fonct.h
+++ fonct.h
@@ -9,9 +9,21 @@
         extern int32_t increaseCompt(int32_t tab[], int32_t n);
         extern int32_t increaseComptThreeMes(int32_t tab[], int32_t n);
 
+
         extern void dayTwo();
+        typedef struct{
+            char dir;
+            int32_t val;
+        }subPos;
         extern int32_t inputJ2[1000][2];
+        //extern subPos inputJ2str[1000];
         extern int32_t posMult(int32_t tab[][2], int32_t n);
         extern int32_t posMultAim(int32_t tab[][2], int32_t n);
+        int32_t posMultAimStruct(subPos * tab, int32_t n);
+
+        extern void dayThree();
+        extern long inputJ3[1000];
+        extern long binCompt(long * bin, int32_t n, int32_t m);
+        extern int32_t binToInt(long l);
 
 #endif
